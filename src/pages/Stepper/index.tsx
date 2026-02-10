84cbc78b6e738442c999f775922d3469dd5bd594
Jennalilu <adobongsiopao@gmail.com>
2026-02-10 17:01:37 +0800

    Create UI for Stepper form

--- src/pages/Stepper/index.tsx
+++ src/pages/Stepper/index.tsx
@@ -1,8 +1,58 @@
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Check, CreditCard, House, UserRoundPen } from "lucide-react"
+import { FormSteps } from "@/types/stepper-form.model"
+import { ReactNode, useState } from "react"
+import { cn } from "@/lib/utils"
+import Button from "@/components/Button"
+import Step1 from "./step1"
+import Step2 from "./Step2"
+import Step3 from "./Step3"
+import Step4 from "./Step4"
+
 export default function StepperForm() {
+	const iconMapper: Record<string, ReactNode> = {
+		info: <UserRoundPen />,
+		address: <House />,
+		pay: <CreditCard />,
+		done: <Check />,
+	}
+	const stepMapper: Record<number, ReactNode> = {
+		0: <Step1 />,
+		1: <Step2 />,
+		2: <Step3 />,
+		3: <Step4 />,
+	}
+	const [currentStep, setCurrentStep] = useState<number>(0)
+
 	return (
 		<div id="stepper-form" className="w-full px-5 mt-3">
-			<h1 className="font-semibold p-3 text-xl w-full">Stepper Form</h1>
-			<hr className="mt-2" />
+			<div className="max-w-2xl mt-5 m-auto">
+				<Card className="pt-0 border border-gray-6 rounded-xl overflow-hidden">
+					<div className="flex justify-center gap-20 bg-gray-50 border-b border-gray-6 py-5">
+						{FormSteps?.map((step) => (
+							<div
+								className={cn("rounded-full bg-gray-200 p-2.5 transition", "[&>svg]:w-5 [&>svg]:h-5 [&>svg]:text-gray-500", {
+									"bg-gray-700 [&>svg]:text-white": currentStep === step.id,
+								})}
+								key={step.id}
+							>
+								{iconMapper?.[`${step.icon}`]}
+							</div>
+						))}
+					</div>
+					<CardContent>{stepMapper?.[currentStep]}</CardContent>
+					<CardFooter>
+						<div className="flex justify-between w-full">
+							{currentStep !== 0 && <Button variant="outline" onClick={() => setCurrentStep((p) => p-1)}>Back</Button>}
+							{currentStep < 3 && (
+								<Button className="ml-auto" onClick={() => setCurrentStep((p) => p+1)}>
+									Next
+								</Button>
+							)}
+						</div>
+					</CardFooter>
+				</Card>
+			</div>
 		</div>
 	)
 }
---
+++ src/types/stepper-form.model.ts
@@ -0,0 +1,22 @@
+export const FormSteps = [
+	{
+		id: 0,
+		name: 'Personal Information',
+		icon: 'info',
+	},
+	{
+		id: 1,
+		name: 'Address',
+		icon: 'address'
+	},
+	{
+		id: 2,
+		name: 'Payment',
+		icon: 'pay'
+	},
+	{
+		id: 3,
+		name: 'Done',
+		icon: 'done'
+	},
+]
